f9f4fecc5adb72f6e57a8c2f5778cd807aa3d2ca
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-14 11:57:21 +0530

    Create 43.java

---
+++ 43.java
@@ -0,0 +1,10 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		System.out.print(s1+s2);
+	}
+}
