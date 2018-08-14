8fc80538cb62278c65240e8a9ef0f83249f0fef8
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-14 11:49:48 +0530

    Create 41.java

---
+++ 41.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i;
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		n=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+			System.out.println(s);
+		}
+	}
+}
