da3eca4c86f5d9d7dae5f9a41f29b7c0954d45d2
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-14 12:01:15 +0530

    Create 44.java

---
+++ 44.java
@@ -0,0 +1,17 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n>1&&n<10)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+	}
+}
