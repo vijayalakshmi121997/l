d574a01621bc71b3ccd836376a547eea29f2398b
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-14 11:53:55 +0530

    Create 42.java

---
+++ 42.java
@@ -0,0 +1,24 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	String s1,s2;
+	    Scanner sc=new Scanner(System.in);
+	    s1=sc.next();
+	    s2=sc.next();
+	    if(s1.length()>s2.length())
+	    {
+	    	System.out.print(s1);
+	    }
+	    else if(s2.length()>s1.length())
+	    {
+	    	System.out.print(s2);
+	 	}
+	 	else 
+	 	{
+	 		System.out.print(s2);
+	 	}
+
+	// your code goes here
+	}
+}
