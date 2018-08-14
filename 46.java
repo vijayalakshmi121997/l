6b12fa6ba887fe6c3ca97700291fbe22aad8811a
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-14 12:30:29 +0530

    Create 46.java

---
+++ 46.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		n=n+1;
+	System.out.println(n);
+  }
+}
