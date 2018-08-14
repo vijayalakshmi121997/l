15a372cfb0b7563570b0b68d31e3d46432eefd61
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-14 12:40:42 +0530

    Create 49.java

---
+++ 49.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	int sum=0;
+	sum=a+b;
+	System.out.println(sum);// your code goes here
+	}
+}
